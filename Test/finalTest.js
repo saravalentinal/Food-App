99ad8e433c2817321a1fdd2ef92c17460050cb9a
LucasSequeDev <lucas.sequeira@yahoo.com.ar>
2022-01-14 15:07:16 -0300

    Fix on presentantion test

--- Test/finalTest.js
+++ Test/finalTest.js
@@ -1,13 +1,10 @@
 const finalTest = ( errors ) => {
 
     try {
-        if (errors.length === 0) {
-            console.log('Felicidades!!! Todos los tests pasaron.')
-        } else {
-            errors.map( error => error.startsWith("#") || error.startsWith("[") || error.startsWith(" ")
+        errors.map( error => error.startsWith("#") || error.startsWith("[") || error.startsWith(" ")
                                     ? console.log( error ) 
                                     : console.log( "# [ ERR ] " + error) )
-        }
+
     } catch (error) {
         console.error("[ ERR-FATAL ] Ocurrio un error en el test final: ", error.message)
     }
